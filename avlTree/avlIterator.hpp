5a14412a0474d5a0ce35be1b1eda593541d48b86
pablomon <pablomonteserin@gmail.com>
2022-04-07 23:55:41 +0200

    avlTree now uses allocator

--- avlTree/avlIterator.hpp
+++ avlTree/avlIterator.hpp
@@ -3,190 +3,196 @@
 
 #include "avlTree.hpp"
 
-template <class N, class P>
-class avlIterator
+namespace ft
 {
-	public:
-	typedef N 						node_type;
-	typedef node_type 				*node_pointer;
+	template <class N, class P>
+	class avlIterator
+	{
+		public:
+		typedef N 						node_type;
+		typedef node_type 				*node_pointer;
 
-	typedef P						value_type;
-	typedef value_type				*pointer;
-	typedef value_type const 		*const_pointer;
-	typedef value_type				&reference;
-	typedef value_type const 		&const_reference;
-	typedef std::ptrdiff_t 			difference_type;
-	typedef std::bidirectional_iterator_tag 	iterator_category;
+		typedef P						value_type;
+		typedef value_type				*pointer;
+		typedef value_type const 		*const_pointer;
+		typedef value_type				&reference;
+		typedef value_type const 		&const_reference;
+		typedef std::ptrdiff_t 			difference_type;
+		typedef std::bidirectional_iterator_tag 	iterator_category;
 
-	private:
-	node_pointer current;
-	node_pointer prev;
-	node_pointer root;
-	int outOfRange = 0;
+		private:
+		node_pointer current;
+		node_pointer prev;
+		node_pointer root;
+		bool outOfRange;
 
-	public:
-	//Default constructor
-    avlIterator(node_pointer root, node_pointer current)
-    {
-        this->root = root;
-		this->current = current;
-		prev = current;
-		if (current == getEnd() + 1)
-			outOfRange = 1;
-    }
-	//Copy
-	avlIterator(avlIterator<node_type, value_type> const &other)
-	{
-		root = other.root;
-		current = other.current;
-		prev = other.prev;
-		outOfRange = other.outOfRange;
-	}
-	//Destructor
-	~avlIterator() {}
-
-	//Operators
-    reference operator*() const { return current->content; }
-	pointer operator->() const { return &current->content; }
-	avlIterator& operator++()
-	{
-		moveForward();
-        return *this;
-	}
-	avlIterator operator++(int)
-	{
-		avlIterator ret = *this;
-   		this->operator++();
-   		return ret;
-	}
-	avlIterator& operator--()
-	{
-		moveBackwards();
-		return *this;
-	}
-	avlIterator operator--(int)
-	{
-		avlIterator ret = *this;
-		this->operator--();
-		return ret;
-	}
-	bool operator==(const avlIterator& other) const
-	{
-		return (other.current == current);
-	}
-	bool operator!=(const avlIterator& other) const
-	{
-		return (other.current != current);
-	}
-
-	// member functions
-	private:
-	node_pointer goFullLeft(node_pointer node)
-	{
-		while (node->left)
-			node = node->left;
-		return node;
-	}
-	node_pointer goFullRight(node_pointer node)
-	{
-		while (node->right)
-			node = node->right;
-		return node;
-	}
-	node_pointer getEnd()
-	{
-		node_pointer node = root;
-		while (node->right)
-			node = node->right;
-		return node;
-	}
-	node_pointer getBegin()
-	{
-		node_pointer node = root;
-		while (node->left)
-			node = node->left;
-		return node;
-	}
-
-	void moveForward()
-	{
-		if (outOfRange)
+		public:
+		//Default constructor
+		avlIterator(node_pointer root, node_pointer current)
 		{
-			current = getEnd();
+			this->root = root;
+			this->current = current;
 			prev = current;
-			outOfRange = 0;
-			return;
+			outOfRange = false;
+			if (current == getEnd() + 1)
+				outOfRange = true;
 		}
-		if (current == getEnd())
+		//Copy
+		avlIterator(avlIterator<node_type, value_type> const &other)
 		{
-			outOfRange = 1;
-			current++;
-			return;
+			root = other.root;
+			current = other.current;
+			prev = other.prev;
+			outOfRange = other.outOfRange;
 		}
-		if (current->right && current->right != prev)
+		//Destructor
+		~avlIterator() {}
+
+		//Operators
+		reference operator*() const { return current->content; }
+		pointer operator->() const { return &current->content; }
+		avlIterator& operator++()
 		{
-			node_pointer node = goFullLeft(current->right);
-			prev = node;
-			current = node;
-			return;
+			moveForward();
+			return *this;
 		}
-		while (current->parent)
+		avlIterator operator++(int)
 		{
-			if (current->parent->content.first > prev->content.first)
+			avlIterator ret = *this;
+			this->operator++();
+			return ret;
+		}
+		avlIterator& operator--()
+		{
+			moveBackwards();
+			return *this;
+		}
+		avlIterator operator--(int)
+		{
+			avlIterator ret = *this;
+			this->operator--();
+			return ret;
+		}
+		bool operator==(const avlIterator& other) const
+		{
+			return (other.current == current);
+		}
+		bool operator!=(const avlIterator& other) const
+		{
+			return (other.current != current);
+		}
+
+		// member functions
+		private:
+		node_pointer goFullLeft(node_pointer node)
+		{
+			while (node->left)
+				node = node->left;
+			return node;
+		}
+		node_pointer goFullRight(node_pointer node)
+		{
+			while (node->right)
+				node = node->right;
+			return node;
+		}
+		node_pointer getEnd()
+		{
+			node_pointer node = root;
+			while (node->right)
+				node = node->right;
+			return node;
+		}
+		node_pointer getBegin()
+		{
+			node_pointer node = root;
+			while (node->left)
+				node = node->left;
+			return node;
+		}
+
+		void moveForward()
+		{
+			if (outOfRange)
 			{
+				current = getEnd();
 				prev = current;
+				outOfRange = false;
+				return;
+			}
+			if (current == getEnd())
+			{
+				outOfRange = true;
+				current++;
+				return;
+			}
+			if (current->right && current->right != prev)
+			{
+				node_pointer node = goFullLeft(current->right);
+				prev = node;
+				current = node;
+				return;
+			}
+			while (current->parent)
+			{
+				if (current->parent->content.first > prev->content.first)
+				{
+					prev = current;
+					current = current->parent;
+					return;
+				}
 				current = current->parent;
+			}
+		}
+	
+		void moveBackwards()
+		{
+			if (outOfRange)
+			{
+				current = getEnd();
+				prev = current;
+				outOfRange = false;
 				return;
 			}
-			current = current->parent;
-		}
-	}
- 
-	void moveBackwards()
-	{
-		if (outOfRange)
-		{
-			current = getEnd();
-			prev = current;
-			outOfRange = 0;
-			return;
-		}
-		if (current == getBegin())
-		{
-			current = getEnd() + 1;
-			prev = current;
-			outOfRange = 1;
-			return;
-		}
-		if (current->right && current->right != prev)
-		{
-			prev = current;
-			current = goFullRight(current);
-			return;
-		}
-		if (current->left && current->left != prev)
-		{
-			if (current->left->right)
+			if (current == getBegin())
 			{
+				current = getEnd() + 1;
 				prev = current;
-				current = goFullRight(current->left->right);
+				outOfRange = true;
 				return;
 			}
-			prev = current;
-			current = current->left;
-			return;
-		}
-		while (current->parent)
-		{
-			if (current->parent->content.first < prev->content.first)
+			if (current->right && current->right != prev)
 			{
 				prev = current;
+				current = goFullRight(current);
+				return;
+			}
+			if (current->left && current->left != prev)
+			{
+				if (current->left->right)
+				{
+					prev = current;
+					current = goFullRight(current->left->right);
+					return;
+				}
+				prev = current;
+				current = current->left;
+				return;
+			}
+			while (current->parent)
+			{
+				if (current->parent->content.first < prev->content.first)
+				{
+					prev = current;
+					current = current->parent;
+					return;
+				}
 				current = current->parent;
-				return;
 			}
-			current = current->parent;
 		}
-	}
-};
+	};
+	
+} // namespace ft
+
 
 #endif // AVLTREE_IT_HPP
--- avlTree/avlTree.hpp
+++ avlTree/avlTree.hpp
@@ -10,351 +10,391 @@
 #include "avlIterator.hpp"
 #include "../utils/pair.hpp"
 
-template <typename K, typename V>
-class AVLTree
+namespace ft
 {
-public:
-	typedef AVLNode<K,V>	node_type;
-	typedef node_type		*node_pointer;
-	typedef avlIterator<node_type, ft::pair<K,V> >	iterator;
-	
-	
-	node_pointer root = NULL;
+	template <typename K, typename V, typename alloc = std::allocator<avlNode <K,V> > >
+	class avlTree
+	{
+	public:
+		typedef alloc									allocator_type;
+		typedef avlNode<K, V> 							node_type;
+		typedef node_type 								*node_pointer;
+		typedef ft::pair<K,V>							content_type;
+		typedef avlIterator<node_type, content_type> 	iterator;
 
-private:
-	void balanceAtNode(AVLNode<K, V> *n)
-	{
-		int bal = n->getBalance();
-		if (bal > 1)
-		{
-			if (n->left->getBalance() < 0)
-				rotateLeft(n->left);
-			rotateRight(n);
-		}
-		else if (bal < -1)
-		{
-			if (n->right->getBalance() > 0)
-				rotateRight(n->right);
-			rotateLeft(n);
-		}
-	}
-	void rotateLeft(AVLNode<K, V> *n)
-	{
-		enum
-		{
-			left,
-			right
-		} side;
-		AVLNode<K, V> *p = n->parent;
-		if (p != NULL && p->left == n)
-			side = left;
-		else
-			side = right;
-		AVLNode<K, V> *temp = n->right;
-		n->setRight(temp->left);
-		temp->setLeft(n);
-		if (p == NULL)
-			setRoot(temp);
-		else if (side == left)
-			p->setLeft(temp);
-		else
-			p->setRight(temp);
-	}
-	void rotateRight(AVLNode<K, V> *n)
-	{
-		enum
-		{
-			left,
-			right
-		} side;
-		AVLNode<K, V> *p = n->parent;
-		if (p != NULL && p->left == n)
-			side = left;
-		else
-			side = right;
-		AVLNode<K, V> *temp = n->left;
-		n->setLeft(temp->right);
-		temp->setRight(n);
-		if (p == NULL)
-			setRoot(temp);
-		else if (side == left)
-			p->setLeft(temp);
-		else
-			p->setRight(temp);
-	}
-	void setRoot(AVLNode<K, V> *n)
-	{
-		root = n;
-		if (root != NULL)
-			root->removeParent();
-	}
+		allocator_type	m_alloc;
+		node_pointer	m_root;
 
-	AVLNode<K, V> *findNode(K key)
-	{
-		AVLNode<K, V> *temp = root;
-		while (temp != NULL)
-		{
-			if (key == temp->content.first)
-				break;
-			else if (key < temp->content.first)
-				temp = temp->left;
-			else
-				temp = temp->right;
-		}
-		return temp;
-	}
-
-	// debug
-	void printSubtree(AVLNode<K, V> *subtree, int depth, int level, bool first)
-	{
-		if (depth > 0)
-		{
-			if (subtree == NULL)
+	public:
+		avlTree() : m_root(NULL) {}
+		avlTree(const avlTree & x) 
+		{ 
+			for (avlTree::iterator it = x.begin(); it < x.end(); it++)
 			{
-				printSubtree(subtree, depth - 1, level, first);
-				printSubtree(subtree, depth - 1, level, false);
-			}
+				std::cout << it->first << std::endl;
+				create_node(*it);
+			}	
+		}
+		~avlTree()
+		{
+			delete_branch(m_root);
+		}
+
+		int getHeight() { return m_root->getHeight(); }
+
+		node_pointer create_node(content_type content)
+		{
+			node_pointer node = m_alloc.allocate(1);
+			m_alloc.construct(node, node_type(content));
+			node->content = content;
+			return node;
+		}
+
+		void delete_node(node_pointer node)
+		{
+			m_alloc.destroy(node);
+			m_alloc.deallocate(node, 1);
+		}
+		void delete_branch(node_pointer node)
+		{
+			if (node == NULL)
+				return;
+			delete_branch(node->left);
+			delete_branch(node->right);
+			delete_node(node);
+		}
+
+		bool insert(content_type pair)
+		{
+			if (m_root == NULL)
+				m_root = create_node(pair);
 			else
 			{
-				printSubtree(subtree->left, depth - 1, level, first);
-				printSubtree(subtree->right, depth - 1, level, false);
-			}
-		}
-		else if (subtree == NULL)
-			std::cout << std::setw((first) ? spacing(level) / 2 : spacing(level)) << "-";
-		else
-			std::cout << std::setw((first) ? spacing(level) / 2 : spacing(level)) << subtree->content.first;
-	}
-	int spacing(int level)
-	{
-		int result = 2;
-		for (int i = 0; i < level; i++)
-			result += result;
-		return result;
-	}
-
-public:
-	AVLTree() : root(NULL) {}
-	AVLTree(ft::pair<K,V> pair) {	root = new AVLNode<K, V>(pair);	}
-
-	int getHeight() { return root->getHeight(); }
-	bool insert(ft::pair<K,V> pair)
-	{
-		if (root == NULL)
-			root = new AVLNode<K, V>(pair);
-		else
-		{
-			AVLNode<K, V> *added_node = NULL;
-			AVLNode<K, V> *temp = root;
-			while (temp != NULL && added_node == NULL)
-			{
-				if (pair.first < temp->content.first)
+				node_pointer added_node = NULL;
+				node_pointer temp = m_root;
+				while (temp != NULL && added_node == NULL)
 				{
-					if (temp->left == NULL)
-						added_node = temp->setLeft(new AVLNode<K, V>(pair));
+					if (pair.first < temp->content.first)
+					{
+						if (temp->left == NULL)
+							added_node = temp->setLeft(create_node(pair));
+						else
+							temp = temp->left;
+					}
+					else if (pair.first > temp->content.first)
+					{
+						if (temp->right == NULL)
+							added_node = temp->setRight(create_node(pair));
+						else
+							temp = temp->right;
+					}
 					else
-						temp = temp->left;
+						return false;
 				}
-				else if (pair.first > temp->content.first)
+
+				temp = added_node;
+				while (temp != NULL)
 				{
-					if (temp->right == NULL)
-						added_node = temp->setRight(new AVLNode<K, V>(pair));
+					temp->updateHeight();
+					balanceAtNode(temp);
+					temp = temp->parent;
+				}
+			}
+			return true;
+		}
+
+		bool remove(K key)
+		{
+			node_pointer toBeRemoved = findNode(key);
+			if (toBeRemoved == NULL)
+				return false;
+			enum
+			{
+				left,
+				right
+			} side;
+			node_pointer p = toBeRemoved->parent;
+			if (p != NULL &&
+				p->left == toBeRemoved)
+				side = left;
+			else
+				side = right;
+			if (toBeRemoved->left == NULL)
+				if (toBeRemoved->right == NULL)
+				{
+					if (p == NULL)
+					{
+						setRoot(NULL);
+						delete_node(toBeRemoved);
+					}
 					else
-						temp = temp->right;
+					{
+						if (side == left)
+							p->setLeft(NULL);
+						else
+							p->setRight(NULL);
+						delete_node(toBeRemoved);
+						p->updateHeight();
+						balanceAtNode(p);
+					}
 				}
 				else
-					return false;
-			}
-
-			temp = added_node;
-			while (temp != NULL)
-			{
-				temp->updateHeight();
-				balanceAtNode(temp);
-				temp = temp->parent;
-			}
-		}
-		return true;
-	}
-	bool remove(K key)
-	{
-		AVLNode<K, V> *toBeRemoved = findNode(key);
-		if (toBeRemoved == NULL)
-			return false;
-		enum
-		{
-			left,
-			right
-		} side;
-		AVLNode<K, V> *p = toBeRemoved->parent;
-		if (p != NULL &&
-			p->left == toBeRemoved)
-			side = left;
-		else
-			side = right;
-		if (toBeRemoved->left == NULL)
-			if (toBeRemoved->right == NULL)
+				{
+					if (p == NULL)
+					{
+						setRoot(toBeRemoved->right);
+						delete_node(toBeRemoved);
+					}
+					else
+					{
+						if (side == left)
+							p->setLeft(toBeRemoved->right);
+						else
+							p->setRight(toBeRemoved->right);
+						delete_node(toBeRemoved);
+						p->updateHeight();
+						balanceAtNode(p);
+					}
+				}
+			else if (toBeRemoved->right == NULL)
 			{
 				if (p == NULL)
 				{
-					setRoot(NULL);
-					delete toBeRemoved;
+					setRoot(toBeRemoved->left);
+					delete_node(toBeRemoved);
 				}
 				else
 				{
 					if (side == left)
-						p->setLeft(NULL);
+						p->setLeft(toBeRemoved->left);
 					else
-						p->setRight(NULL);
-					delete toBeRemoved;
+						p->setRight(toBeRemoved->left);
+					delete_node(toBeRemoved);
 					p->updateHeight();
 					balanceAtNode(p);
 				}
 			}
 			else
 			{
-				if (p == NULL)
+				node_pointer replacement;
+				node_pointer replacement_parent;
+				node_pointer temp_node;
+				int bal = toBeRemoved->getBalance();
+				if (bal > 0)
 				{
-					setRoot(toBeRemoved->right);
-					delete toBeRemoved;
-				}
-				else
-				{
-					if (side == left)
-						p->setLeft(toBeRemoved->right);
+					if (toBeRemoved->left->right == NULL)
+					{
+						replacement = toBeRemoved->left;
+						replacement->setRight(toBeRemoved->right);
+						temp_node = replacement;
+					}
 					else
-						p->setRight(toBeRemoved->right);
-					delete toBeRemoved;
-					p->updateHeight();
-					balanceAtNode(p);
+					{
+						replacement = toBeRemoved->left->right;
+						while (replacement->right != NULL)
+							replacement = replacement->right;
+						replacement_parent = replacement->parent;
+						replacement_parent->setRight(replacement->left);
+						temp_node = replacement_parent;
+						replacement->setLeft(toBeRemoved->left);
+						replacement->setRight(toBeRemoved->right);
+					}
 				}
-			}
-		else if (toBeRemoved->right == NULL)
-		{
-			if (p == NULL)
-			{
-				setRoot(toBeRemoved->left);
-				delete toBeRemoved;
-			}
-			else
-			{
-				if (side == left)
-					p->setLeft(toBeRemoved->left);
-				else
-					p->setRight(toBeRemoved->left);
-				delete toBeRemoved;
-				p->updateHeight();
-				balanceAtNode(p);
-			}
-		}
-		else
-		{
-			AVLNode<K, V> *replacement;
-			AVLNode<K, V> *replacement_parent;
-			AVLNode<K, V> *temp_node;
-			int bal = toBeRemoved->getBalance();
-			if (bal > 0)
-			{
-				if (toBeRemoved->left->right == NULL)
+				else if (toBeRemoved->right->left == NULL)
 				{
-					replacement = toBeRemoved->left;
-					replacement->setRight(toBeRemoved->right);
+					replacement = toBeRemoved->right;
+					replacement->setLeft(toBeRemoved->left);
 					temp_node = replacement;
 				}
 				else
 				{
-					replacement = toBeRemoved->left->right;
-					while (replacement->right != NULL)
-						replacement = replacement->right;
+					replacement = toBeRemoved->right->left;
+					while (replacement->left != NULL)
+						replacement = replacement->left;
+
 					replacement_parent = replacement->parent;
-					replacement_parent->setRight(replacement->left);
+					replacement_parent->setLeft(replacement->right);
 					temp_node = replacement_parent;
 					replacement->setLeft(toBeRemoved->left);
 					replacement->setRight(toBeRemoved->right);
 				}
+				if (p == NULL)
+					setRoot(replacement);
+				else if (side == left)
+					p->setLeft(replacement);
+				else
+					p->setRight(replacement);
+				delete_node(toBeRemoved);
+				balanceAtNode(temp_node);
 			}
-			else if (toBeRemoved->right->left == NULL)
+			return true;
+		}
+		V getValue(K key)
+		{
+			node_pointer n = findNode(key);
+			if (n == NULL)
+				return 0;
+			return n->content.second;
+		}
+		node_pointer getFirst()
+		{
+			if (m_root == NULL)
+				return NULL;
+			node_pointer node = m_root;
+			while (node->left != NULL)
+				node = node->left;
+			return node;
+		}
+
+		iterator begin()
+		{
+			iterator it(m_root, getFirst());
+			return it;
+		}
+
+		node_pointer getLast()
+		{
+			if (m_root == NULL)
+				return NULL;
+			node_pointer node = m_root;
+			while (node->right != NULL)
+				node = node->right;
+			return node;
+		}
+		iterator end()
+		{
+			iterator it(m_root, getLast() + 1);
+			return it;
+		}
+
+		// debug
+		void print()
+		{
+			if (m_root == NULL)
 			{
-				replacement = toBeRemoved->right;
-				replacement->setLeft(toBeRemoved->left);
-				temp_node = replacement;
+				std::cout << "Empty" << std::endl;
+				return;
 			}
+			int max = m_root->getHeight();
+			for (int depth = 0; depth <= max; depth++)
+			{
+				printSubtree(m_root, depth, max - depth + 1, true);
+				std::cout << std::endl;
+			}
+		}
+
+	private:
+		void balanceAtNode(node_pointer n)
+		{
+			int bal = n->getBalance();
+			if (bal > 1)
+			{
+				if (n->left->getBalance() < 0)
+					rotateLeft(n->left);
+				rotateRight(n);
+			}
+			else if (bal < -1)
+			{
+				if (n->right->getBalance() > 0)
+					rotateRight(n->right);
+				rotateLeft(n);
+			}
+		}
+		void rotateLeft(node_pointer n)
+		{
+			enum
+			{
+				left,
+				right
+			} side;
+			node_pointer p = n->parent;
+			if (p != NULL && p->left == n)
+				side = left;
 			else
-			{
-				replacement = toBeRemoved->right->left;
-				while (replacement->left != NULL)
-					replacement = replacement->left;
-				
-				replacement_parent = replacement->parent;
-				replacement_parent->setLeft(replacement->right);
-				temp_node = replacement_parent;
-				replacement->setLeft(toBeRemoved->left);
-				replacement->setRight(toBeRemoved->right);
-			}
+				side = right;
+			node_pointer temp = n->right;
+			n->setRight(temp->left);
+			temp->setLeft(n);
 			if (p == NULL)
-				setRoot(replacement);
+				setRoot(temp);
 			else if (side == left)
-				p->setLeft(replacement);
+				p->setLeft(temp);
 			else
-				p->setRight(replacement);
-			delete toBeRemoved;
-			balanceAtNode(temp_node);
+				p->setRight(temp);
 		}
-		return true;
-	}
-	V getValue(K key)
-	{
-		AVLNode<K,V> *n = findNode(key);
-		if (n == NULL)
-			return 0;
-		return n->content.second;
-	}
-	node_pointer getFirst()
-	{
-		if (root == NULL)
-			return NULL;
-		node_pointer node = root;
-		while(node->left != NULL)
-			node = node->left;
-		return node;
-	}
-
-	iterator begin()
-	{
-		iterator it(root, getFirst());
-		return it;
-	}
-
-	node_pointer getLast()
-	{
-		if (root == NULL)
-			return NULL;
-		node_pointer node = root;
-		while(node->right != NULL)
-			node = node->right;
-		return node;
-	}
-	iterator end()
-	{
-		iterator it(root, getLast() + 1);
-		return it;
-	}
-
-	// debug
-	void print()
-	{
-		if (root == NULL)
+		void rotateRight(node_pointer n)
 		{
-			std::cout << "Empty" << std::endl;
-			return;
+			enum
+			{
+				left,
+				right
+			} side;
+			node_pointer p = n->parent;
+			if (p != NULL && p->left == n)
+				side = left;
+			else
+				side = right;
+			node_pointer temp = n->left;
+			n->setLeft(temp->right);
+			temp->setRight(n);
+			if (p == NULL)
+				setRoot(temp);
+			else if (side == left)
+				p->setLeft(temp);
+			else
+				p->setRight(temp);
 		}
-		int max = root->getHeight();
-		for (int depth = 0; depth <= max; depth++)
+		void setRoot(node_pointer n)
 		{
-			printSubtree(root, depth, max - depth + 1, true);
-			std::cout << std::endl;
+			m_root = n;
+			if (m_root != NULL)
+				m_root->removeParent();
 		}
-	}
-};
 
+		node_pointer findNode(K key)
+		{
+			node_pointer temp = m_root;
+			while (temp != NULL)
+			{
+				if (key == temp->content.first)
+					break;
+				else if (key < temp->content.first)
+					temp = temp->left;
+				else
+					temp = temp->right;
+			}
+			return temp;
+		}
+
+		// debug
+		void printSubtree(node_pointer subtree, int depth, int level, bool first)
+		{
+			if (depth > 0)
+			{
+				if (subtree == NULL)
+				{
+					printSubtree(subtree, depth - 1, level, first);
+					printSubtree(subtree, depth - 1, level, false);
+				}
+				else
+				{
+					printSubtree(subtree->left, depth - 1, level, first);
+					printSubtree(subtree->right, depth - 1, level, false);
+				}
+			}
+			else if (subtree == NULL)
+				std::cout << std::setw((first) ? spacing(level) / 2 : spacing(level)) << "-";
+			else
+				std::cout << std::setw((first) ? spacing(level) / 2 : spacing(level)) << subtree->content.first;
+		}
+		int spacing(int level)
+		{
+			int result = 2;
+			for (int i = 0; i < level; i++)
+				result += result;
+			return result;
+		}
+	};
+
+} // ft
 #endif // AVL_HPP
